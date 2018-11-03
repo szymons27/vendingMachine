ce397d9e19a2555f98a1d26dcc423c5de7400c29
Szymon Sygiet <sz.sygiet@gmail.com>
2018-11-03 13:50:33 +0100

    fourth

--- src/main/java/pl/sdaacademy/vending/Main.java
+++ src/main/java/pl/sdaacademy/vending/Main.java
@@ -1,9 +1,15 @@
 package pl.sdaacademy.vending;
 
+import pl.sdaacademy.vending.model.VendingMachine;
+import pl.sdaacademy.vending.util.Configuration;
 import pl.sdaacademy.vending.vending.CustomerOperationController;
 
 public class Main {
     public static void main(String[] args) {
-        new CustomerOperationController().printMachine();
+        Configuration configuration = new Configuration();
+        VendingMachine vendingMachine = new VendingMachine(configuration);
+        CustomerOperationController customerOperationController = new CustomerOperationController(vendingMachine);
+
+        customerOperationController.printMachine();
     }
 }
--- src/main/java/pl/sdaacademy/vending/vending/CustomerOperationController.java
+++ src/main/java/pl/sdaacademy/vending/vending/CustomerOperationController.java
@@ -3,10 +3,10 @@ package pl.sdaacademy.vending.vending;
 import pl.sdaacademy.vending.model.VendingMachine;
 
 public class CustomerOperationController {
-    private VendingMachine machine;
+    private final VendingMachine machine;
 
-    public CustomerOperationController() {
-        machine = new VendingMachine();
+    public CustomerOperationController(VendingMachine machine) {
+        this.machine = machine;
     }
 
     public void printMachine() {
