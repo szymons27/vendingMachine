867ce27eec71e0148d497a2dffcffb73b82a8262
Szymon Sygiet <sz.sygiet@gmail.com>
2018-11-04 10:32:15 +0100

    seven

--- src/main/java/pl/sdaacademy/vending/model/Tray.java
+++ src/main/java/pl/sdaacademy/vending/model/Tray.java
@@ -1,13 +1,55 @@
 package pl.sdaacademy.vending.model;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class Tray {
     private String symbol;
+    private Long price;
+    private Queue<Product> products;
 
-    public Tray(String symbol) {
-        this.symbol = symbol;
+    public Tray(Builder builder) {
+        symbol = builder.symbol;
+        price = builder.price;
+        products = builder.products;
+
     }
+
+    public static Builder builder(String symbol){return new Builder(symbol);}
 
     public String getSymbol() {
         return symbol;
     }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public static class Builder {
+        private String symbol;
+        private Long price;
+        private Queue<Product> products;
+
+        private Builder(String symbol){
+            this.symbol = symbol;
+            products = new ArrayDeque<>();
+        }
+
+        public Builder product(Product product){
+            products.add(product);
+            return this;
+        }
+
+        public Builder price(Long price){
+            this.price = price;
+            return this;
+        }
+        public Tray build(){
+            if(price == null || price < 0){
+                price = 0L;
+            }
+            return new Tray(this);
+        }
+    }
+
 }
--- src/main/java/pl/sdaacademy/vending/model/VendingMachine.java
+++ src/main/java/pl/sdaacademy/vending/model/VendingMachine.java
@@ -3,6 +3,7 @@ package pl.sdaacademy.vending.model;
 import pl.sdaacademy.vending.util.Configuration;
 
 import java.util.Optional;
+import java.util.Random;
 
 public class VendingMachine {
 
@@ -25,26 +26,49 @@ public class VendingMachine {
         if (colsCount <= 0 || colsCount > 9) {
             throw new IllegalArgumentException("Col count" + colsCount + "is invalid");
         }
-        //stworzyc tablice 2 wymiarowa
-        //do kazdego pola tablicy wpisac nowy obiekt tacki
-        //obiekt tacki musi miec ustawiony poprawny symbol
 
         trays = new Tray[rowsCount.intValue()][colsCount.intValue()];
+        Random random = new Random();
         for (int rowNumber = 0; rowNumber < rowsCount; rowNumber++) {
             for (int colNumber = 0; colNumber < trays.length; colNumber++) {
-                char symbolLetter = (char) ('A' + rowNumber);
-                int symbolNumber = colNumber + 1;
-                String symbol = "" + symbolLetter + symbolNumber;
-                trays[rowNumber][colNumber] = new Tray(symbol);
+                if (Math.random() < 0.8) {
+                    generateTrayAtPosition(rowNumber, colNumber);
+                    if (Math.random() < 0.5) {
+                        if (Math.random() < 0.1) {
+                        }
+                    }
+                }
             }
         }
     }
+
+    private void generateTrayAtPosition(int rowNumber, int colNumber) {
+        Random random = new Random();
+        long price = random.nextInt(901) + 100;
+        char symbolLetter = (char) ('A' + rowNumber);
+        int symbolNumber = colNumber + 1;
+        String symbol = "" + symbolLetter + symbolNumber;
+
+        Tray.Builder trayBuilder = Tray.builder(symbol).price(price);
+        int productProbability = random.nextInt(10);
+        if (productProbability < 5){
+            trayBuilder = trayBuilder
+                    .product(new Product("Product " + symbol));
+        }
+        if (productProbability < 1){
+            trayBuilder = trayBuilder
+                    .product(new Product("Product" + symbol));
+        }
+        trays[rowNumber][colNumber] = trayBuilder.build();
+
+    }
+
     public Optional<Tray> getTrayAtPosition(int rowNumber, int colNumber) {
         try {
             Tray tray = trays[rowNumber][colNumber];
             Optional<Tray> wrappedTray = Optional.ofNullable(tray);
             return wrappedTray;
-        } catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             return Optional.empty();
         }
     }
@@ -56,9 +80,33 @@ public class VendingMachine {
     }
 
     public Long colsCount() {
-
         return configuration.getLongProperty(
                 "machine.size.cols",
                 4L);
     }
 }
+//        if (productProbability < 1) {
+//        Tray tray = Tray
+//        .builder(symbol)
+//        .price(price)
+//        .product(new Product("Product " + symbol))
+//        .product(new Product("Product " + symbol))
+//        .build();
+//        trays[rowNumber][colNumber] = tray;
+//        //2 produkt
+//        } else if (productProbability < 5) {
+//        Tray tray = Tray
+//        .builder(symbol)
+//        .price(price)
+//        .product(new Product("Product " + symbol))
+//        .build();
+//        trays[rowNumber][colNumber] = tray;
+//        //1 produkt
+//        } else {
+//        Tray tray = Tray
+//        .builder(symbol)
+//        .price(price)
+//        .build();
+//        trays[rowNumber][colNumber] = tray;
+//        //0 produktów
+//        }
