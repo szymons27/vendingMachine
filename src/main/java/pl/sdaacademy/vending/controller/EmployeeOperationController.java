29013bd7fb97adaf0fc2ab4140cddd754ee0612c
Szymon Sygiet <sz.sygiet@gmail.com>
2018-11-18 09:44:42 +0100

    getTrayPriceFromUser

--- src/main/java/pl/sdaacademy/vending/controller/EmployeeOperationController.java
+++ src/main/java/pl/sdaacademy/vending/controller/EmployeeOperationController.java
@@ -1,25 +1,43 @@
 package pl.sdaacademy.vending.controller;
 
+import pl.sdaacademy.vending.controller.service.EmployeeService;
 import pl.sdaacademy.vending.model.Tray;
 
 import java.util.Optional;
 import java.util.Scanner;
 
 public class EmployeeOperationController {
-    public void addTray(){
-        System.out.println("insert tray symbol");
-        String employeeInput1 = new Scanner(System.in).nextLine();
-        Integer traySymbolNumber = Integer.valueOf(employeeInput1);
-        System.out.println("insert tray price");
-        String employeeInput2 = new Scanner(System.in).nextLine();
-        Long trayPrice = Long.valueOf(employeeInput2);
-        Tray.builder(employeeInput1)
-                .price(trayPrice)
-                .build();
-        //delegate tray save to service
-        //print confirmation or error
+    private final EmployeeService employeeService;
 
-        Optional<String> messageToPrint;
-        System.out.println(messageToPrint.orElse("success"));
+    public EmployeeOperationController(EmployeeService employeeService){this.employeeService = employeeService;}
+
+    public void addTray(){
+        String traySymbol = getTraySymbolFromUser();
+        System.out.println(" > Provide tray symbol: ");
+        String symbol = getUserInput();
+        System.out.println(" > Provide tray price");
+        Long price = Long.parseLong(getUserInput());
+        Long trayPice = getTrayPriceFromUser();
+        Tray newTray = Tray.builder(symbol).price(price).build();
+        Optional<String> errorMessage = employeeService.addTray(newTray);
+        System.out.println(errorMessage.orElse("success"));
+    }
+
+    private Long getTrayPriceFromUser() {
+        Long price = null;
+        while (price == null){
+            System.out.println(" > Provide tray price");
+            Long.parseLong(getUserInput());
+        }
+        return price;
+    }
+
+    private String getTraySymbolFromUser(){
+        System.out.println(" > Provide tray symbol: ");
+        return getUserInput().toUpperCase();
+    }
+
+    private String getUserInput() {
+        return new Scanner(System.in).nextLine();
     }
 }
